907bf19ed0b2983dc78ed9f8c66f151c7e3f1a47
JulioJosueG <jh2019-0417@uce.edu.do>
2022-03-11 11:19:09 -0400

    Reportes y  Mejora de menu Principal

--- WebSchoolAppUI/Startup.cs
+++ WebSchoolAppUI/Startup.cs
@@ -50,7 +50,7 @@ namespace WebSchoolAppUI
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Login}/{id?}");
             });
         }
     }
