8bfced335b57304f943867148a8cac723b789a61
JulioJosueG <jh2019-0417@uce.edu.do>
2022-04-21 22:10:37 -0400

    Cambios en Cubo

---
+++ WebSchoolAppUI/Models/SchoolYear.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSchoolAppUI.Models
+{
+    public class SchoolYear
+    {
+        public string Year { get; set; }
+    }
+}
